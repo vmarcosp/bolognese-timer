02daf724e6100553c46d20b07a4729f466028ab9
vmarcosp <contato@vmarcosp.com.br>
2017-10-10 23:55:41 -0300

    Removed muse-ui

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue';
-import MuseUI from 'muse-ui';
-import 'muse-ui/dist/muse-ui.css';
+import 'normalize.css';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
-
-Vue.use(MuseUI);
 
 /* eslint-disable no-new */
 new Vue({
