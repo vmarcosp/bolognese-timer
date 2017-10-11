9bdcbf7c44fe8464e2209068544dac243ac4ec04
vmarcosp <contato@vmarcosp.com.br>
2017-10-11 00:51:02 -0300

    Added logo in the app bar

--- src/components/b-bar/BBar.vue
+++ src/components/b-bar/BBar.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="container">
-    <h1>Teste</h1>
+    <!-- <img class="logo" :src="pathLogo"> -->
+    <span>Bolognese Timer</span>
   </div>
 </template>
 
 <script>
+import logo from '@/assets/images/logo.svg';
+
 export default {
   name: 'BBar',
+  data() {
+    return {
+      pathLogo: logo,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+  text-transform: uppercase;
+  padding: 1.5rem 0;
+  display: flex;
+  font-weight: bold;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, .2);
+  color: white;
+  text-align: center;
+  font-size: 30px;
+  .logo {
+    height: 50px;
+  }
+}
 </style>
