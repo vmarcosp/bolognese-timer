7a8568d228d981620047fb090309e2e7aa6d52ca
Marcos Vinicius <contato@vmarcosp.com.br>
2017-10-13 20:01:44 -0300

    Created modal component

---
+++ src/components/b-modal-settings/BModalSettings.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BModalSettings'
+};
+</script>
+
+<style lang="scss" scoped>
+.container{
+  position: fixed;
+  top:0;
+  left: 0;
+}
+</style>
---
+++ src/components/b-modal-settings/index.js
@@ -0,0 +1,1 @@
+export { default as BModalSettings } from './BModalSettings';
